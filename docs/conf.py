13d4a37d79be32cdb94e1032016c6937e40db68f
Your Name <grafuls@gmail.com>
2025-04-25 13:24:56 +0000

    Bump version: 0.1.52 → 0.1.53

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ project = "quads-lib"
 year = "2025"
 author = "Gonzalo Rafuls"
 copyright = f"{year}, {author}"
-version = release = "0.1.52"
+version = release = "0.1.53"
 
 pygments_style = "trac"
 templates_path = ["."]
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(*names, **kwargs):
 
 setup(
     name="quads-lib",
-    version="0.1.52",
+    version="0.1.53",
     license="LGPL-3.0-only",
     description="Python client library for interacting with the QUADS API",
     long_description="{}\n{}".format(
