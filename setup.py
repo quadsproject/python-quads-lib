218c6e4f4eabbbd69e83fdb927d007081a14af3d
Your Name <grafuls@gmail.com>
2025-04-28 11:53:23 +0000

    Bump version: 0.1.78 → 0.1.79

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(*names, **kwargs):
 
 setup(
     name="quads-lib",
-    version="0.1.78",
+    version="0.1.79",
     license="LGPL-3.0-only",
     description="Python client library for interacting with the QUADS API",
     long_description="{}\n{}".format(
