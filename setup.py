0c1afa4a4d7b84ee6546596d0afef3f061e772a8
Your Name <grafuls@gmail.com>
2025-04-28 12:04:11 +0000

    Bump version: 0.1.107 → 0.1.108

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(*names, **kwargs):
 
 setup(
     name="quads-lib",
-    version="0.1.107",
+    version="0.1.108",
     license="LGPL-3.0-only",
     description="Python client library for interacting with the QUADS API",
     long_description="{}\n{}".format(
--- src/quads_lib/__init__.py
+++ src/quads_lib/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.107"
+__version__ = "0.1.108"
 
 from .quads import QuadsApi
 
