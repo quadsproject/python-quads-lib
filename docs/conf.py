a75e3774373a23d33de2b4c1791a988c9a1d1f29
Your Name <grafuls@gmail.com>
2025-04-28 12:38:29 +0000

    Bump version: 0.1.191 → 0.1.192

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ project = "quads-lib"
 year = "2025"
 author = "Gonzalo Rafuls"
 copyright = f"{year}, {author}"
-version = release = "0.1.191"
+version = release = "0.1.192"
 
 pygments_style = "trac"
 templates_path = ["."]
--- src/quads_lib/__init__.py
+++ src/quads_lib/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.191"
+__version__ = "0.1.192"
 
 from .quads import QuadsApi
 
