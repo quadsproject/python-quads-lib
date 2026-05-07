ae038a615f9067a5b398784ceacb7bc4708a4d15
Will Foster <wfoster@redhat.com>
2026-05-07 22:01:17 +0100

    fix: update CI frameworks [publish]

--- src/quads_lib/base.py
+++ src/quads_lib/base.py
@@ -56,7 +56,9 @@ class QuadsBase:
         self.logout()
         self.session.close()
 
-    def _make_request(self, method: str, endpoint: str, data: Optional[dict] = None) -> dict:
+    def _make_request(
+        self, method: str, endpoint: str, data: Optional[dict] = None
+    ) -> dict:
         full_url = urljoin(self.base_url, endpoint)
         _response = self.session.request(
             method,
--- src/quads_lib/quads.py
+++ src/quads_lib/quads.py
@@ -208,7 +208,9 @@ class QuadsApi(QuadsBase):
     def create_assignment(self, data: dict) -> dict:
         response = self.post("assignments", data)
         if response and {"id", "cloud"} <= response.keys():
-            print(f"Assignment created - ID: {response['id']}, Cloud: {response['cloud']['name']}")
+            print(
+                f"Assignment created - ID: {response['id']}, Cloud: {response['cloud']['name']}"
+            )
         return response
 
     @returns("Assignment")
@@ -216,7 +218,9 @@ class QuadsApi(QuadsBase):
         endpoint = Path("assignments") / "self"
         response = self.post(str(endpoint), data)
         if response and {"id", "cloud"} <= response.keys():
-            print(f"Self-assignment created - ID: {response['id']}, Cloud: {response['cloud']['name']}")
+            print(
+                f"Self-assignment created - ID: {response['id']}, Cloud: {response['cloud']['name']}"
+            )
         return response
 
     @returns("Assignment")
